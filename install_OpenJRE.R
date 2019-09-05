daf9ed3522cb6606cfc630cdc8c589f29b3718fc
Mārtiņš Liberts <martins.liberts@csb.gov.lv>
2019-09-05 15:19:47 +0300

    Iespēja izvēlēties Java provaideri.

--- install_OpenJRE.R
+++ install_OpenJRE.R
@@ -5,38 +5,104 @@ require(rvest)
 require(openssl)
 
 
-install.open.jre <- function(path.jre = file.path(Sys.getenv("HOME"), "OpenJRE"),
-                             set.env.variable = TRUE) {
+install.open.jre <- function(path.jre = file.path(Sys.getenv("HOME"),
+                                                  "OpenJRE"),
+                             set.env.variable = TRUE,
+                             provider = "amazon") {
 
+  provider <- tolower(el(as.character(provider)))
+
+  if (!(provider %in% c("adopt", "zulu", "amazon"))) {
+    stop("Nepareizs JRE provider nosaukums")
+  }
+
+  if (provider == "adopt") {
+    cat("Instalējam AdoptOpenJDK, https://adoptopenjdk.net/ \n")
+  } else if (provider == "zulu") {
+    cat("Instalējam Zulu JDK, https://www.azul.com/downloads/zulu/ \n")
+  } else if (provider == "amazon") {
+    cat("Instalējam Amazon Corretto, https://aws.amazon.com/corretto/ \n")
+  }
+
+  cat("Instalācija tiek veikta folderī:", path.jre, "\n")
   dir.create(path.jre, showWarnings = FALSE)
 
-  # AdoptOpenJDK/openjdk8-binaries
-  html.doc <- read_html("https://github.com/AdoptOpenJDK/openjdk8-binaries/releases")
+  if (provider == "adopt") {
+    base.url <- "https://github.com"
+    html.doc <- read_html(file.path(base.url,
+                                    "AdoptOpenJDK/openjdk8-binaries/releases"))
+  } else if (provider == "zulu") {
+    base.url <- "https://cdn.azul.com/zulu/bin"
+    html.doc <- read_html(base.url)
+  } else if (provider == "amazon") {
+    base.url <- NA_character_
+    html.doc <- read_html("https://docs.aws.amazon.com/corretto/latest/corretto-8-ug/downloads-list.html")
+  }
+
   url.list <- html_nodes(html.doc, "a") %>% html_attr(name = "href")
-  down.list <- grep("OpenJDK8U-jre_x64_windows_hotspot_8u.*(zip|zip.sha256.txt)$", url.list, value = T)
 
-  zip.name <- el(basename(grep("zip$", down.list, value = T)))
-  txt.name <- el(basename(grep("txt$", down.list, value = T)))
+  if (provider == "adopt") {
+    down.list <- grep("jre_x64_windows_hotspot_8u.*(zip|zip.sha256.txt)$",
+                      url.list, value = T)
+  } else if (provider == "zulu") {
+    down.list <- grep("fx-jre8.*-win_x64.zip$", url.list, value = T)
+  } else if (provider == "amazon") {
+    down.list <- grep("windows-x64-jre.zip$", url.list, value = T)
+  }
 
-  for (i in 1:2) download.file(url = file.path("https://github.com", down.list[i]),
-                               destfile = file.path(path.jre, basename(down.list[i])),
-                               method = "wininet")
+  down.list <- sort(down.list, decreasing = T)
+
+  zip.name <- el(grep("zip$", down.list, value = T))
+  zip.base.name <- basename(zip.name)
+
+  cat("Instalācijas fails:", zip.base.name, "\n")
+
+  if (is.na(base.url)) {
+    down.url <- zip.name
+  } else {
+    down.url <- file.path(base.url, zip.name)
+  }
+
+  if (!file.exists(file.path(path.jre, zip.base.name))) {
+    download.file(url = down.url,
+                  destfile = file.path(path.jre, zip.base.name),
+                  method = "wininet")
+  }
 
   # Pārbaude
-  checksum <- read.table(file.path(path.jre, txt.name))$V1
-  con <- file(file.path(path.jre, zip.name))
-  if (gsub(":", "", sha256(x = con)) != checksum) stop("Neparezs fails")
+  if (provider == "adopt") {
+    txt.name <- el(grep("txt$", down.list, value = T))
+
+    if (!file.exists(file.path(path.jre, basename(txt.name)))) {
+      download.file(url = file.path(base.url, txt.name),
+                    destfile = file.path(path.jre, basename(txt.name)),
+                    method = "wininet")
+    }
+
+    checksum <- read.table(file.path(path.jre, basename(txt.name)))$V1
+    con <- file(file.path(path.jre, zip.base.name))
+    if (gsub(":", "", sha256(x = con)) != checksum) stop("Neparezs fails")
+  }
 
 
   # unzip
-  unzip(zipfile = file.path(path.jre, zip.name), exdir = path.jre)
-
+  dir.name <- file.path(path.jre,
+                        unzip(zipfile = file.path(path.jre, zip.base.name),
+                              list = T)[1, "Name"])
+  if (dir.exists(dir.name)) unlink(dir.name, recursive = T)
+  unzip(zipfile = file.path(path.jre, zip.base.name), exdir = path.jre)
 
   # set JAVA_HOME
   if (set.env.variable) {
-    java_home <- normalizePath(tail(list.dirs(path = path.jre, full.names = T, recursive = F), 1))
+    java_home <- normalizePath(dir.name)
     system(command = paste("setx JAVA_HOME", java_home))
+    cat("JAVA_HOME:", java_home, "\n")
   }
+
+  cat("Instalācija ir pabeigta\n")
 }
 
-install.open.jre()
+# install.open.jre()
+# install.open.jre(provider = "adopt")
+# install.open.jre(provider = "amazon")
+# install.open.jre(provider = "zulu")
